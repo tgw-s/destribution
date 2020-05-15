1fd10e2f2a6c039a4c61f176ccccc0509fc5b32b
ShotaTogawa <s.togawa0112@gmail.com>
2020-05-15 16:46:32 +0900

    introduced react-router-dom

--- src/pages/TopMain.tsx
+++ src/pages/TopMain.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import family from '../assets/family.jpg';
 import Paper from '@material-ui/core/Paper';
@@ -25,13 +26,24 @@ const useStyle = makeStyles(() =>
 
 const TopMain: FC = () => {
   const classes = useStyle();
+  const history = useHistory();
+  const [keyword, setKeyword] = useState('');
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+  const handleSubmit = () => {
+    history.push('/search/' + keyword);
+  };
   return (
     <div className={classes.background}>
-      <Paper className={classes.paper}>
+      <Paper className={classes.paper} component="form" onSubmit={handleSubmit}>
         <IconButton type="submit">
           <SearchIcon />
         </IconButton>
-        <InputBase placeholder="search for free materials" />
+        <InputBase
+          onChange={handleChange}
+          placeholder="search for free materials"
+        />
       </Paper>
     </div>
   );
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -2,13 +2,21 @@
 import React, { FC } from 'react';
 import TopHeader from '../components/topPage/TopHeader';
 import TopMain from './TopMain';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const TopPage: FC = () => {
   return (
-    <>
-      <TopHeader />
-      <TopMain />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <TopHeader />
+          <TopMain />
+        </Route>
+        <Route path="/search/:keyword" exact>
+          検索結果
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
