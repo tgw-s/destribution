7a08b6e443c6ed0be1659628e84d4d85cdfff1cb
ShotaTogawa <s.togawa0112@gmail.com>
2020-05-15 15:48:30 +0900

    added TopPage

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TopPage from './pages/TopPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TopPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/TopPage.tsx
@@ -0,0 +1,8 @@
+// FCはfunctional componentを作るためのモジュール
+import React, { FC } from 'react';
+
+const TopPage: FC = () => {
+  return <h2>Top Page</h2>;
+};
+
+export default TopPage;
